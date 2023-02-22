dab37a1f90ac3d6ef28e95bc643574270f61133b
dbstarll <dbstarll@gmail.com>
2023-02-22 12:01:50 +0800

    com.fasterxml.jackson的JsonApiClient增加更多结果类型的转换

--- src/main/java/io/github/dbstarll/utils/json/jackson/JsonApiClient.java
+++ src/main/java/io/github/dbstarll/utils/json/jackson/JsonApiClient.java
@@ -1,12 +1,37 @@
 package io.github.dbstarll.utils.json.jackson;
 
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.dbstarll.utils.net.api.ApiClient;
+import io.github.dbstarll.utils.net.api.ApiException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+
+import java.io.IOException;
+import java.util.List;
 
 public abstract class JsonApiClient extends ApiClient {
+    protected final ObjectMapper mapper;
+
     protected JsonApiClient(final HttpClient httpClient, final boolean alwaysProcessEntity, final ObjectMapper mapper) {
         super(httpClient);
+        this.mapper = mapper;
         setResponseHandlerFactory(new JsonResponseHandlerFactory(mapper, alwaysProcessEntity));
+    }
+
+    protected final <T> T execute(final HttpUriRequest request, final JavaType javaType)
+            throws IOException, ApiException {
+        return mapper.convertValue(super.execute(request, JsonNode.class), javaType);
+    }
+
+    protected final <T> T executeObject(final HttpUriRequest request, final Class<T> responseClass)
+            throws IOException, ApiException {
+        return execute(request, mapper.getTypeFactory().constructType(responseClass));
+    }
+
+    protected final <T> List<T> executeArray(final HttpUriRequest request, final Class<T> responseClass)
+            throws IOException, ApiException {
+        return execute(request, mapper.getTypeFactory().constructCollectionType(List.class, responseClass));
     }
 }
--- src/test/java/io/github/dbstarll/utils/json/jackson/JsonApiClientTest.java
+++ src/test/java/io/github/dbstarll/utils/json/jackson/JsonApiClientTest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.github.dbstarll.utils.http.client.request.RelativeUriResolver;
 import io.github.dbstarll.utils.json.JsonApiClientTestCase;
 import io.github.dbstarll.utils.json.ThrowingBiConsumer;
+import io.github.dbstarll.utils.json.test.Model;
 import io.github.dbstarll.utils.net.api.ApiException;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
@@ -17,7 +18,9 @@ import org.junit.jupiter.api.function.ThrowingConsumer;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -66,6 +69,19 @@ class JsonApiClientTest extends JsonApiClientTestCase {
     }
 
     @Test
+    void model() throws Throwable {
+        useApi((s, c) -> {
+            final Model model = c.model();
+            assertNotNull(model);
+            assertEquals(100, model.getIntValue());
+            assertEquals("stringValue1", model.getStringValue());
+            assertTrue(model.isBooleanValue());
+            assertEquals(3.14, model.getFloatValue(), 0.0001);
+            assertArrayEquals(new int[]{1, 2, 3, 4, 5}, model.getIntArray());
+        }, s -> s.enqueue(new MockResponse().setBody(jsonObject)));
+    }
+
+    @Test
     void array() throws Throwable {
         useApi((s, c) -> {
             final ArrayNode array = c.array();
@@ -82,6 +98,22 @@ class JsonApiClientTest extends JsonApiClientTestCase {
         }, s -> s.enqueue(new MockResponse().setBody(jsonArray)));
     }
 
+    @Test
+    void models() throws Throwable {
+        useApi((s, c) -> {
+            final List<Model> array = c.models();
+            assertNotNull(array);
+            assertEquals(2, array.size());
+
+            final Model model = array.get(1);
+            assertEquals(101, model.getIntValue());
+            assertEquals("stringValue2", model.getStringValue());
+            assertFalse(model.isBooleanValue());
+            assertEquals(1.41, model.getFloatValue(), 0.0001);
+            assertArrayEquals(new int[]{5, 4, 3, 2, 1}, model.getIntArray());
+        }, s -> s.enqueue(new MockResponse().setBody(jsonArray)));
+    }
+
     private static class MyApiClient extends JsonApiClient {
         public MyApiClient(final HttpClient httpClient, final String uriBase, final ObjectMapper mapper) {
             super(httpClient, true, mapper);
@@ -92,8 +124,16 @@ class JsonApiClientTest extends JsonApiClientTestCase {
             return execute(get("/ping.html").build(), ObjectNode.class);
         }
 
+        public Model model() throws ApiException, IOException {
+            return executeObject(get("/ping.html").build(), Model.class);
+        }
+
         public ArrayNode array() throws ApiException, IOException {
             return execute(get("/ping.html").build(), ArrayNode.class);
+        }
+
+        public List<Model> models() throws ApiException, IOException {
+            return executeArray(get("/ping.html").build(), Model.class);
         }
     }
 }
