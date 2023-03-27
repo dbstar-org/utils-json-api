9fdd8a539600e8f6dae8ae1082885b76328f3a22
dbstarll <dbstarll@gmail.com>
2023-03-27 10:37:38 +0800

    范型名称合规

--- src/main/java/io/github/dbstarll/utils/json/jackson/StreamJavaTypeResponseHandler.java
+++ src/main/java/io/github/dbstarll/utils/json/jackson/StreamJavaTypeResponseHandler.java
@@ -10,15 +10,15 @@ import org.apache.hc.core5.http.io.HttpClientResponseHandler;
 import java.io.IOException;
 import java.util.function.BiConsumer;
 
-final class StreamJavaTypeResponseHandler<H extends Index<HD>, HD, TD>
-        implements HttpClientResponseHandler<JavaTypeIndex<TD>> {
+final class StreamJavaTypeResponseHandler<H extends Index<H1>, H1, T>
+        implements HttpClientResponseHandler<JavaTypeIndex<T>> {
     private final ObjectMapper mapper;
     private final HttpClientResponseHandler<H> responseHandler;
     private final JavaType javaType;
-    private final BiConsumer<HD, TD> consumer;
+    private final BiConsumer<H1, T> consumer;
 
     StreamJavaTypeResponseHandler(final ObjectMapper mapper, final HttpClientResponseHandler<H> responseHandler,
-                                  final JavaType javaType, final BiConsumer<HD, TD> consumer) {
+                                  final JavaType javaType, final BiConsumer<H1, T> consumer) {
         this.mapper = mapper;
         this.responseHandler = responseHandler;
         this.javaType = javaType;
@@ -26,12 +26,12 @@ final class StreamJavaTypeResponseHandler<H extends Index<HD>, HD, TD>
     }
 
     @Override
-    public JavaTypeIndex<TD> handleResponse(final ClassicHttpResponse response) throws HttpException, IOException {
+    public JavaTypeIndex<T> handleResponse(final ClassicHttpResponse response) throws HttpException, IOException {
         final H handlerResult = responseHandler.handleResponse(response);
         if (handlerResult.getData() == null) {
             return new JavaTypeIndex<>(null, handlerResult.getIndex());
         } else {
-            final TD convertResult = mapper.convertValue(handlerResult.getData(), javaType);
+            final T convertResult = mapper.convertValue(handlerResult.getData(), javaType);
             consumer.accept(handlerResult.getData(), convertResult);
             return new JavaTypeIndex<>(convertResult, handlerResult.getIndex());
         }
