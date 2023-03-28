d362ae7b6fbf53bbb8af8206c32fad828aba6ca4
dbstarll <dbstarll@gmail.com>
2023-03-29 00:19:15 +0800

    去掉JavaTypeIndex

--- src/main/java/io/github/dbstarll/utils/json/jackson/JavaTypeIndex.java
+++
@@ -1,9 +0,0 @@
-package io.github.dbstarll.utils.json.jackson;
-
-import io.github.dbstarll.utils.net.api.index.AbstractIndex;
-
-class JavaTypeIndex<T> extends AbstractIndex<T> {
-    JavaTypeIndex(final T data, final int index) {
-        super(data, index);
-    }
-}
--- src/main/java/io/github/dbstarll/utils/json/jackson/StreamJavaTypeResponseHandler.java
+++ src/main/java/io/github/dbstarll/utils/json/jackson/StreamJavaTypeResponseHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.dbstarll.utils.json.JsonParseException;
+import io.github.dbstarll.utils.net.api.index.AbstractIndex;
 import io.github.dbstarll.utils.net.api.index.Index;
 import io.github.dbstarll.utils.net.api.index.IndexBaseHttpClientResponseHandler;
 import org.apache.commons.lang3.StringUtils;
@@ -26,13 +27,15 @@ final class StreamJavaTypeResponseHandler<T> extends IndexBaseHttpClientResponse
     @Override
     protected Index<T> handleContent(final String content, final boolean endOfStream) throws IOException {
         if (StringUtils.isBlank(content)) {
-            return new JavaTypeIndex<>(null, -1);
+            return new AbstractIndex<T>(null, -1) {
+            };
         }
         try (JsonParser parser = mapper.createParser(content)) {
             final T data = mapper.readValue(parser, javaType);
             final int index = (int) parser.currentLocation().getCharOffset();
             consumer.accept(content.substring(0, index), data);
-            return new JavaTypeIndex<>(data, index);
+            return new AbstractIndex<T>(data, index) {
+            };
         } catch (Exception e) {
             if (endOfStream) {
                 throw new JsonParseException(e);
